59bdd3635571a431489c4a40f5bd628dab63a0d6
Myoung Jin Jang <thinkjin6@gmail.com>
2021-09-19 15:31:12 +0900

    Update ValidateBinarySearchTree.js

--- solutions/medium/ValidateBinarySearchTree.js
+++ solutions/medium/ValidateBinarySearchTree.js
@@ -24,7 +24,7 @@
 
 var isValidBST = function(root, min = null, max = null) {
     if (!root) return true;
-    if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) return false;
+    if ((min && root.val <= min.val) || (max && root.val >= max.val)) return false;
     
-    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max); 
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max); 
 };
